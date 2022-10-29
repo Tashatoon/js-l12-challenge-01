aaa6d094a182db8ea0733289b8ff2cffd6b4add3
Natasha <111096866+Tashatoon@users.noreply.github.com>
2022-10-29 15:38:44 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,6 +6,23 @@ const getImage = async function () {
     const res = await fetch(
         "https://picsum.photos/v2/list?limit=100");
 const images = await res.json();
+    selectRandomImage(images);
 console.log(images);
 };
-getImage();
+const selectRandomImage = function (images) {
+    const randonIndex = Math.floor(Math.random() * images.length);
+    console.log(randomIndex);
+    randomImages = images[randomIndex];
+    console.log(randomImages);
+    displayImage = images[randomIndex];
+};
+const displayImage = function (randomImage) {
+    const author = randomIndex.author;
+   const imageAddress = randomIndex.download_url;
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imaDiv.classlist.remove = "hide";
+};
+  button.addeventListener("click", function (){
+  getImage();
+  });
